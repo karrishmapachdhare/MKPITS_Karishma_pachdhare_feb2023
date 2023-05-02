724532e4ef4e98daed836ad4189db3e44562cac1
karrishmapachdhare <124714749+karrishmapachdhare@users.noreply.github.com>
2023-05-02 10:28:59 +0530

    file upload

---
+++ sql programs/SQLQuery1.sql
@@ -0,0 +1,15 @@
+create table student(rollno int,name varchar(25))
+Insert Into Student values(1,'karishma')
+Insert Into Student values(2,'kajal')
+Insert Into Student values(3,'tina')
+Insert Into Student values(4,'diksha')
+Insert Into Student values(5,'mona')
+Insert Into Student values(6,'anjali')
+Insert Into Student values(7,'tanu')
+Insert Into Student values(8,'jay')
+Insert Into Student values(9,'rudra')
+Insert Into Student values(10,'rupali')
+Insert Into Student values(11,'shreya')
+select*from student 
+
+
