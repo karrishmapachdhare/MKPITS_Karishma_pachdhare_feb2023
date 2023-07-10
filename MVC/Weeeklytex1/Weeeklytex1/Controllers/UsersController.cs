73d22b0811875499d9287857bce5a65bf97c55b4
karrishmapachdhare <124714749+karrishmapachdhare@users.noreply.github.com>
2023-07-10 13:16:01 +0530

    file upload

---
+++ MVC/Weeeklytex1/Weeeklytex1/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Weeeklytex1.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Users
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Weeklytex1(int txtId, string txtName, string chkAddon)
+        {
+            ViewBag.Id = txtId;
+            ViewBag.Name = txtName;
+            if (chkAddon != null)
+                ViewBag.Addon = "Selected";
+            else
+                ViewBag.Addon = "Not Selected";
+        }
+}
---
+++ MVC/Weeeklytex1/Weeeklytex1/Models/StudentModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Weeeklytex1.Models
+{
+    public class StudentModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool addon { get; set; }
+    }
+}
